d4af9c735e34aca62bafe27b2fdff663ef4e9518
mchao409 <mchao409@gmail.com>
2018-07-09 22:20:29 -0400

    add class names to function args

--- svg_element_creator.js
+++ svg_element_creator.js
@@ -2,35 +2,35 @@
 
 svgns = "http://www.w3.org/2000/svg";
 
-function createPath(point_1, point_2, color, svg_name) {
+function createPath(point_1, point_2, svg_name, class_name) {
 	var path = document.createElementNS(svgns, "path");
 	var input = "M" + point_1[0] + "," + point_1[1] + 
 			" L" + point_2[0] + "," + point_2[1];
 	path.setAttributeNS(null, 'd', input);
-	path.setAttribute("stroke", color);
+	path.setAttributeNS(null, 'class', class_name);
 	document.getElementById(svg_name).appendChild(path);
+	return path;
 }
 
-function createCircle(cx, cy, radius, color, stroke, svg_name) {
+function createCircle(cx, cy, radius, svg_name, class_name) {
+	var circle = document.createElementNS(svgns, 'circle');
 	circle.setAttributeNS(null, 'cx', cx);
 	circle.setAttributeNS(null, 'cy', cy);
 	circle.setAttributeNS(null, 'r', radius);
-	circle.style.fill = color;
-	circle.style.stroke = stroke;
-	circle.style.strokeWidth = 1;
+	circle.setAttributeNS(null, 'class', class_name);
 	document.getElementById(svg_name).appendChild(circle);
+	return circle;
 }
 
-function createRectangle(x,y,width,height,fill, stroke,svg_name) {
+function createRectangle(x,y,width,height,svg_name, class_name) {
 	var rect = document.createElementNS(svgns, 'rect');
 	rect.setAttributeNS(null,'x', x);
 	rect.setAttributeNS(null, 'y', y);
 	rect.setAttributeNS(null, 'height', height);
 	rect.setAttributeNS(null,'width', width);
-	rect.style.fill = fill;
-	rect.style.stroke = stroke;
-	rect.style.strokeWidth = 1;
+	rect.setAttributeNS(null, "class", class_name);
 	document.getElementById(svg_name).appendChild(rect);
+	return rect;
 }
 
 
